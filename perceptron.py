29b77a01215813558c25102149c10bbcd1b48270
Remco Volmer <rvolmer@valburg.science.ru.nl>
2019-09-17 10:20:42 +0200

    Added update counting matrix

--- perceptron.py
+++ perceptron.py
@@ -11,20 +11,28 @@ class Perceptron():
         return weights
 
     def predict(self, x_u):
-        return self.__sign(np.sum(self.weights[1:]*x_u) + self.weights[0])
+        x_u = x_u.insert(0,1)
+        return self.__sign(sum(self.weights*x_u))
 
     def fit(self, x, t):
         if len(x[0]) != self.dim:
             print('Error: dimensions of input ({}) do not match to dimensions of Perceptron ({})'.format(len(x[0]), self.dim))
-        z = x * t
-        finished_fit = False
-        while !finished_fit:
-            for i in range(self.dim):
-                
-        pass
 
-    def __update_weights(self):
-        pass
+        for i in range(len(x)):
+            x[i] = x[i].insert(0,1) # Add 1 at zero position of input so weight_0 can be included in sum
+
+        M = [0] * len(x)
+        z = x * t
+        M_old = []
+        while M_old != M:
+            M_old = M
+            for i in range(self.dim):
+                if self.weights*z[i]<0:
+                    self.__update_weights(z[i])
+                    M[i] += 1
+
+    def __update_weights(self, z_i):
+        self.weights += self.learning_rate * z_i
 
     def __sign(self, a):
         if a >=0:
