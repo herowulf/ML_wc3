b1607c6533a7b97dd535a8a35853e3ca5019afea
herowulf <50174623+herowulf@users.noreply.github.com>
2019-09-17 08:50:07 +0200

    Add files via upload

---
+++ perceptron.py
@@ -0,0 +1,16 @@
+class Perceptron():
+    def __init__(self, input_dim):
+        self.input_dim = input_dim
+        self.weights = self.__initialize_weights(self.input_dim)
+
+    def __initialize_weights(self, input_dim):
+        pass
+
+    def predict(self):
+        pass
+
+    def fit(self):
+        pass
+
+    def __update_weights(self):
+        pass
