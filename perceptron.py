7f76f894d8472637abd8a298083b9290f8f9fafd
Remco Volmer <rvolmer@valburg.science.ru.nl>
2019-09-17 10:10:59 +0200

    Implement perceptron

--- perceptron.py
+++ perceptron.py
@@ -1,18 +1,33 @@
 import numpy as np
 
 class Perceptron():
-    def __init__(self, input_dim):
-        self.input_dim = input_dim
-        self.weights = self.__initialize_weights(self.input_dim)
+    def __init__(self, dim):
+        self.dim = dim
+        self.weights = self.__initialize_weights(self.dim)
+        self.learning_rate = 1
 
-    def __initialize_weights(self, input_dim):
-        pass
+    def __initialize_weights(self, dim):
+        weights = np.random.random(dim+1)
+        return weights
 
-    def predict(self):
-        pass
+    def predict(self, x_u):
+        return self.__sign(np.sum(self.weights[1:]*x_u) + self.weights[0])
 
-    def fit(self):
+    def fit(self, x, t):
+        if len(x[0]) != self.dim:
+            print('Error: dimensions of input ({}) do not match to dimensions of Perceptron ({})'.format(len(x[0]), self.dim))
+        z = x * t
+        finished_fit = False
+        while !finished_fit:
+            for i in range(self.dim):
+                
         pass
 
     def __update_weights(self):
         pass
+
+    def __sign(self, a):
+        if a >=0:
+            return 1
+        else:
+            return -1
